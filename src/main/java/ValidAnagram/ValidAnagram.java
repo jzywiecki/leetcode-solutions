17cb062b225944903fa7cdfbf2d70b729dd4bd25
jzywiecki <jakubzywiecki1@gmail.com>
2023-08-01 00:24:29 +0200

    Added valid anagram solution using sorting array

--- src/main/java/ValidAnagram/ValidAnagram.java
+++ src/main/java/ValidAnagram/ValidAnagram.java
@@ -1,2 +1,23 @@
-package ValidAnagram;public class ValidAnagram {
+package ValidAnagram;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.sort;
+
+public class ValidAnagram {
+    public static boolean validAnagramSolution(String s, String t){
+        if (s.length() != t.length()){
+            return false;
+        }
+
+        char[] sChars = s.toCharArray();
+        char[] tChars= t.toCharArray();
+
+        sort(sChars);
+        sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
 }
